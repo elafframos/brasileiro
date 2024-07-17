cd9a49a732e442c9a69a5df22f7944cadf1203a0
Élaff Ramos <valelaff10@gmail.com>
2024-07-17 10:21:08 -0300

    Create brasileiro.js

---
+++ brasileiro.js
@@ -0,0 +1,14 @@
+    <script>
+        function nascimento() {
+            var pais = document.querySelector('input#txtr')
+            var con = window.document.querySelector('div#con')
+            var n = String(pais.value)
+
+            if (n == 'brasileiro') {
+                con.innerHTML += `Você nasceu no Brasil!`
+            } else {
+                con.innerHTML += `Você não nasceu no Brasil!`
+            }
+
+        }
+    </script>

21dd03a7f7dbbfcb114e5e06d6e29e7cdb806cd3
Élaff Ramos <valelaff10@gmail.com>
2024-07-18 10:35:58 -0300

    Update brasileiro.js

--- brasileiro.js
+++ brasileiro.js
@@ -1,14 +1,12 @@
-    <script>
-        function nascimento() {
-            var pais = document.querySelector('input#txtr')
-            var con = window.document.querySelector('div#con')
-            var n = String(pais.value)
+function nascimento() {
+    var pais = document.querySelector('input#txtr')
+    var con = window.document.querySelector('div#con')
+    var n = String(pais.value)
 
-            if (n == 'brasileiro') {
-                con.innerHTML += `Você nasceu no Brasil!`
-            } else {
-                con.innerHTML += `Você não nasceu no Brasil!`
-            }
+    if (n == 'brasileiro') {
+        con.innerHTML += `Você nasceu no Brasil!`
+    } else {
+        con.innerHTML += `Você não nasceu no Brasil!`
+    }
 
-        }
-    </script>
+}
